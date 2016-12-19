4e2d47608b34f82c3e75f7cdd3449b09c4746d15
Kristijan Korać <korac.kristijan@gmail.com>
2016-12-19 13:08:21 +0100

    service upgrade- deleting temp and zip files functions

--- INFOS-NXChecker-service/Service1.cs
+++ INFOS-NXChecker-service/Service1.cs
@@ -18,9 +18,12 @@ namespace INFOS_NXChecker_service
     public partial class NXCheckerService : ServiceBase
     {
         #region Variables
-        private String period;
-        private String path;
-        private Timer tmr = new Timer();
+        string period;
+        string path;
+        string tempPath1;
+        string tempPath2;
+        string tempPath3;
+        Timer tmr = new Timer();
         #endregion
 
         public NXCheckerService()
@@ -34,14 +37,18 @@ namespace INFOS_NXChecker_service
             {
                 period   = HelperMethods.GetSubKey(RegistryNames.period, false);
                 path     = HelperMethods.GetSubKey(RegistryNames.path, false);
+                
+                tempPath1   = HelperMethods.GetSubKey(RegistryNames.pathTemp1, false);
+                tempPath2   = HelperMethods.GetSubKey(RegistryNames.pathTemp2, false);
+                tempPath3   = HelperMethods.GetSubKey(RegistryNames.pathTemp3, false);
 
-                tmr.Interval    = Convert.ToInt16(period);
+                tmr.Interval    = Convert.ToInt32(period);
                 tmr.Elapsed     += Tmr_Elapsed;
                 tmr.Start();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("GREŠKA: " + ex.Message);
+                File.WriteAllText(@"C:\Users\Kristijan\Desktop\greska_log-" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt", "ERROR PRI ČITANJU: " + ex.Message);
             }
         }
 
@@ -49,8 +56,15 @@ namespace INFOS_NXChecker_service
         {
             try
             {
-                DirectoryInfo dir   = new DirectoryInfo(path);
-                FileInfo[] zipFiles = dir.GetFiles("*.zip");
+                DirectoryInfo backupDir     = new DirectoryInfo(path);
+                DirectoryInfo tempOneDir    = new DirectoryInfo(tempPath1);
+                DirectoryInfo tempTwoDir    = new DirectoryInfo(tempPath2);
+                DirectoryInfo tempThreeDir  = new DirectoryInfo(tempPath3);
+
+                FileInfo[] zipFiles         = backupDir.GetFiles("*.zip");
+                FileInfo[] tempOneFiles     = tempOneDir.GetFiles();
+                FileInfo[] tempTwoFiles     = tempOneDir.GetFiles();
+                FileInfo[] tempThreeFiles   = tempOneDir.GetFiles();
 
                 zipFiles            = zipFiles.OrderByDescending(file => file.CreationTime).ToArray();
                 FileInfo latestZip  = zipFiles[0];
@@ -76,6 +90,13 @@ namespace INFOS_NXChecker_service
                 //    text += "; \n";
                 //}
 
+                //Deleting files from TEMP locations
+                DeleteTempFiles(tempPath1);
+                DeleteTempFiles(tempPath2);
+                DeleteTempFiles(tempPath3);
+
+                DeleteOldBackupFiles(path);
+
                 string logPath = path + @"\NXChecker_service_" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".nxch";
                 File.WriteAllText(@logPath, text);
 
@@ -84,6 +105,47 @@ namespace INFOS_NXChecker_service
             catch (Exception ex)
             {
                 File.WriteAllText(@"C:\Users\Kristijan\Desktop\greska_log-" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt", "Dogodila se GREŠKA: " + ex.Message);
+            }
+        }
+
+        private void DeleteTempFiles(string tempPath)
+        {
+            if (Directory.Exists(tempPath))
+            {
+                foreach (string file in Directory.GetFiles(tempPath))
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception ex)
+                    {
+                        File.WriteAllText(@"C:\Users\Kristijan\Desktop\deleteFile-" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt", "Dogodila se GREŠKA: " + ex.Message);
+                    }
+                }
+            }     
+        }
+
+        private void DeleteOldBackupFiles(string backupPath)
+        {
+            try
+            {
+                if (Directory.Exists(backupPath))
+                {
+                    FileInfo[] zipFiles = new DirectoryInfo(@backupPath).GetFiles("*.zip");
+                    File.WriteAllText(@"C:\Users\Kristijan\Desktop\zips-" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt", "zips su tu: " + zipFiles.ToString());
+                    foreach (FileInfo file in zipFiles)
+                    {
+                        if (DateTime.UtcNow - file.CreationTimeUtc > TimeSpan.FromDays(3))
+                        {
+                            File.Delete(file.FullName);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"C:\Users\Kristijan\Desktop\deleteFile-" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt", "Greška kod brisanja Backup file-ova: " + ex.Message);
             }
         }
 
